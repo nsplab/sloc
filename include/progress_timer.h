a66f2e075e200bf014f409d3b4e00bce2bca1f9a
Luis Armendariz <luis.armendariz@gmail.com>
2011-12-24 02:23:58 -0800

    Added ProgressTimer class

---
+++ include/progress_timer.h
@@ -0,0 +1,36 @@
+#ifndef PROGRESS_TIMER_H
+#define PROGRESS_TIMER_H
+
+#include <ctime>
+#include <iostream>
+#include <string>
+
+class ProgressTimer
+{
+public:
+    ProgressTimer();
+    ~ProgressTimer();
+
+    std::string header(const char *firstcol);
+
+    ProgressTimer& start(int total);
+    ProgressTimer& update(int current);
+
+
+public:
+    int total;
+    int current;
+    time_t t_0, t_n;
+    double elapsed_mins;
+    double mins_per_num;
+    double num_per_sec;
+    double remaining_mins;
+    double total_mins;
+    double progress;
+};
+
+
+std::ostream& operator<<(std::ostream& os, const ProgressTimer& T);
+
+
+#endif
