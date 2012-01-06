592f66f4d19eb99821d4bdcb93f23e95bfd5c0e1
Luis Armendariz <luis.armendariz@gmail.com>
2012-01-06 00:46:22 -0800

    Process smaller file first: Artery2.stl
    Also, added a small test case for debugging tri2quad()

--- tests/skull.cc
+++ tests/skull.cc
@@ -16,17 +16,32 @@ int main(void)
 {
     Mesh tmesh, qmesh;
 
-    // process Bone2.stl
-    tmesh.read_stl("tmp/Bone2.stl");
-    tri2quad(tmesh, qmesh);
-    tmesh.write_ucd("tmp/Bone2.tri.ucd");
-    qmesh.write_ucd("tmp/Bone2.quad.ucd");
+    if (false)
+    {
+        // quick test
+        tmesh.read_ucd("tmp/tri.ucd");
+        tri2quad(tmesh, qmesh);
+        tmesh.write_ucd("tmp/test-tri.inp");
+        qmesh.write_ucd("tmp/test-quad.inp");
+    }
 
-    // process Artery2.stl
-    tmesh.read_stl("tmp/Artery2.stl");
-    tri2quad(tmesh, qmesh);
-    tmesh.write_ucd("tmp/Artery2.tri.ucd");
-    qmesh.write_ucd("tmp/Artery2.quad.ucd");
+    if (true)
+    {
+        // process Artery2.stl
+        tmesh.read_stl("tmp/Artery2.stl");
+        tri2quad(tmesh, qmesh);
+        tmesh.write_ucd("tmp/Artery2.tri.ucd");
+        qmesh.write_ucd("tmp/Artery2.quad.ucd");
+    }
+
+    if (true)
+    {
+        // process Bone2.stl
+        tmesh.read_stl("tmp/Bone2.stl");
+        tri2quad(tmesh, qmesh);
+        tmesh.write_ucd("tmp/Bone2.tri.ucd");
+        qmesh.write_ucd("tmp/Bone2.quad.ucd");
+    }
 
     return 0;
 }
