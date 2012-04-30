e2f563adffe6d008f01f37510c31a433942c9065
Luis Armendariz <luis.armendariz@gmail.com>
2012-04-30 11:57:22 -0700

    Updated run.sh and added surf.sh to case1 example

--- examples/case1/run.sh
+++ examples/case1/run.sh
@@ -1,7 +1,18 @@
 #!/bin/bash -x
+
+# first, create the surface meshes we need
+./surf.sh
+
+# next, build the true solutions using the true_dpN.dipole files
 ./case1.py prep fwd
 ./case1.py run fwd > ./fwd.sh
-./fwd.sh 
+chmod +x ./fwd.sh
+time ./fwd.sh
+
+# now try to do source localization on the noisy data
 ./case1.py prep inv
 ./case1.py run inv > ./inv.sh
+chmod +x ./inv.sh
 time ./inv.sh 2>&1 | tee inv.log
+
+# EOF
