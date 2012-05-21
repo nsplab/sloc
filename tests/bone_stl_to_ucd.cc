c8afb9bfa692c52acf2a5bc2fd1f2e868f59ac72
Luis Armendariz <luis.armendariz@gmail.com>
2012-05-20 17:02:55 -0700

    Generalized bone_stl_to_ucd.cc to accept command line arguments

--- tests/bone_stl_to_ucd.cc
+++ tests/bone_stl_to_ucd.cc
@@ -3,45 +3,58 @@
  * Program to convert Bone2.stl and Artery2.stl into .ucd files
  * In the process, we also need to convert the corresponding
  * triangle mesh into a quadrilateral mesh.
+ *
+ * Use this program to convert:
+ *   Artery2.stl -> Artery2.quad.ucd
+ *   Bone2.stl -> Bone2.quad.ucd
  */
 #include <iostream>
 #include <ctime>
 #include "mesh.h"
 #include "geometry_utils.h"
+#include <boost/algorithm/string/predicate.hpp>
 
 using namespace std;
 using namespace sloc;
 
-int main(void)
+int main(int argc, char *argv[])
 {
     Mesh tmesh, qmesh;
 
-    if (false)
+    if (argc == 1)
     {
-        // quick test
-        tmesh.read_ucd("tmp/tri.ucd");
-        tri2quad(tmesh, qmesh);
-        tmesh.write_ucd("tmp/test-tri.inp");
-        qmesh.write_ucd("tmp/test-quad.inp");
+        cout << "Usage: " << argv[0] << " file.stl" << endl;
+        return 0;
     }
 
-    if (true)
+    string infile, prefix;
+    string outfile1, outfile2;
+
+    if (boost::algorithm::ends_with(argv[1], ".stl"))
     {
-        // process Artery2.stl
-        tmesh.read_stl("tmp/Artery2.stl");
-        tri2quad(tmesh, qmesh);
-        tmesh.write_ucd("tmp/Artery2.tri.ucd");
-        qmesh.write_ucd("tmp/Artery2.quad.ucd");
+        infile = argv[1];
+        prefix = infile.substr(0, infile.size()-4);
+    }
+    else
+    {
+        prefix = argv[1];
+        infile = prefix + ".stl";
     }
 
-    if (true)
-    {
-        // process Bone2.stl
-        tmesh.read_stl("tmp/Bone2.stl");
-        tri2quad(tmesh, qmesh);
-        tmesh.write_ucd("tmp/Bone2.tri.ucd");
-        qmesh.write_ucd("tmp/Bone2.quad.ucd");
-    }
+    outfile1 = prefix + ".tri.ucd";
+    outfile2 = prefix + ".quad.ucd";
+
+    cout << "Reading " << infile << endl;
+    tmesh.read_stl(infile.c_str());
+
+    cout << "Converting triangles to quads..." << endl;
+    tri2quad(tmesh, qmesh);
+
+    cout << "Writing " << outfile1 << endl;
+    tmesh.write_ucd(outfile1.c_str());
+
+    cout << "Writing " << outfile2 << endl;
+    qmesh.write_ucd(outfile2.c_str());
 
     return 0;
 }
