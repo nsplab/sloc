96645f29969198ca02be909a2e4a51308a0d7045
Luis Armendariz <luis.armendariz@gmail.com>
2012-06-12 16:59:13 -0700

    Added color_utils.h

---
+++ include/color_utils.h
@@ -0,0 +1,24 @@
+#ifndef SLOC_COLOR_UTILS_H
+#define SLOC_COLOR_UTILS_H
+
+// See http://en.wikipedia.org/wiki/ANSI_escape_code
+
+#define ANSI_RESET          "\033[0m"
+#define ANSI_BLACK          "\033[30m"
+#define ANSI_RED            "\033[31m"
+#define ANSI_GREEN          "\033[32m"
+#define ANSI_YELLOW         "\033[33m"
+#define ANSI_BLUE           "\033[34m"
+#define ANSI_MAGENTA        "\033[35m"
+#define ANSI_CYAN           "\033[36m"
+#define ANSI_WHITE          "\033[37m"
+#define ANSI_BOLD_BLACK     "\033[1m\033[30m"
+#define ANSI_BOLD_RED       "\033[1m\033[31m"
+#define ANSI_BOLD_GREEN     "\033[1m\033[32m"
+#define ANSI_BOLD_YELLOW    "\033[1m\033[33m"
+#define ANSI_BOLD_BLUE      "\033[1m\033[34m"
+#define ANSI_BOLD_MAGENTA   "\033[1m\033[35m"
+#define ANSI_BOLD_CYAN      "\033[1m\033[36m"
+#define ANSI_BOLD_WHITE     "\033[1m\033[37m"
+
+#endif
