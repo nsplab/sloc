9b3dc466f32178d986860ed942147ac45c53c320
Luis Armendariz <luis.armendariz@gmail.com>
2012-06-20 12:14:29 -0700

    Updated sphere4.run.sh in examples/simple/

--- examples/simple/sphere4.run.sh
+++ examples/simple/sphere4.run.sh
@@ -1,6 +1,27 @@
 #!/bin/bash -x
-./tests/spherical_head_model 0
+
+# prepare the mesh
+./tests/spherical_head_model \
+    --debug=0 \
+    --refinement-level=0 \
+    --output-mesh=./out/sphere4.surf.ucd
+
+# solve the forward model using a specific dipole
 ./bin/bem_solve sphere4.fwd.prm
-./bin/select_electrodes -m sphere4.surf.ucd -o sphere4.ec1 1/10 4/30
-./bin/measure_electrodes -p sphere4_phi.dat -e sphere4.ec1 -o sphere4.em1
-./bin/bem_inverse_solver sphere4.inv.prm | tee sphere4.inv.log
+
+# select electrodes
+if [ ! -f ./sphere4.ec1 ]; then
+    ./bin/select_electrodes \
+        -m sphere4.surf.ucd \
+        -o sphere4.ec1 \
+        1/10 4/30
+fi
+
+# add noise to the electrodes
+#./bin/measure_electrodes -p sphere4_phi.dat -e sphere4.ec1 -o sphere4.em1
+./bin/measure_electrodes \
+    -p sphere4_phi.dat \
+    -e sphere4.ec1 \
+    -o sphere4.em1
+
+#./bin/bem_inverse_solver sphere4.inv.prm | tee sphere4.inv.log
