22868bd7f4e8796886b04b52e84f2f3d13ab1a57
Luis Armendariz <luis.armendariz@gmail.com>
2012-11-19 16:33:34 -0800

    Added icosahedron.run.sh to /data/simple/ directory

---
+++ data/simple/icosahedron.run.sh
@@ -0,0 +1,32 @@
+#!/bin/bash -x
+
+N=1
+if [[ -n "$1" ]]; then
+    N=$1
+fi
+
+./make_dipoles \
+    -v icosahedron.dipoles.vtk -o icosahedron.dipoles \
+    0,0,0/0,0,1
+
+./select_electrodes \
+    -m icosahedron.surf.mesh -i icosahedron.surf.mat \
+    -v icosahedron.electrodes.vtk -o icosahedron.electrodes \
+    4/12
+
+cat >icosahedron.fwd.prm <<DOC
+# icosahedron.fwd.prm
+set verbose = true
+set debug = true
+
+set surface_mesh = icosahedron.surf.mesh
+set surface_mesh_materials = icosahedron.surf.mat 
+set material_data = icosahedron.sigma
+set dipole_sources = icosahedron.dipoles
+
+set output_vtk = icosahedron.phi.vtk
+set output_phi = icosahedron.phi.dat
+DOC
+
+mpiexec -n $N ./bem_forward_solver icosahedron.fwd.prm >icosahedron.log
+
